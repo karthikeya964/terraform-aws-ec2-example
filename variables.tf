499fb011368076b7d2c177fc710ef7c5332ed739
karthikeya964 <126311771+karthikeya964@users.noreply.github.com>
2025-05-05 15:27:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,16 @@
+variable "ami" {
+  description = "The AMI to use for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of instance to start"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "name" {
+  description = "The name tag for the instance"
+  type        = string
+}
 
